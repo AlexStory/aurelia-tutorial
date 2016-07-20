2e818a59491e977fd9f404db70ea02d0f011e7aa
Story <astory@umpublishing.org>
2016-07-20 13:25:22 -0500

    recursive routing

--- src/app.js
+++ src/app.js
@@ -6,7 +6,21 @@ export class App {
         name: 'welcome',
         moduleId: './welcome',
         nav: true,
-        title:'Welcome' }
+        title:'Welcome' },
+      {
+        route: 'users',
+        name: 'users',
+        moduleId: './users',
+        nav: true,
+        title: 'Users'
+    },
+    {
+        route: 'child-router',
+        name: 'child-router',
+        moduleId: 'child-router',
+        nav: true,
+        title: 'Child Router'
+    }
     ]);
 
     this.router = router;
